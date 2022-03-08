a8383abd70cdcc6696e1b80a0d9f744f9b12ea1f
Abhaya V <abhayav.cs18@bmsce.ac.in>
2022-03-08 18:27:40 +0530

    Add files via upload

--- lib/pages/myprofile/myprofile.dart
+++ lib/pages/myprofile/myprofile.dart
@@ -1,119 +1,92 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:hmsf_intern/pages/Dashboard/dashboard_page.dart';
+import 'package:my_demo_app/pages/Dashboard/dashboard_page.dart';
+import 'package:my_demo_app/widgets/my_button.dart';
 
 class Myprofile extends StatelessWidget
 {
-  @override
-  Widget lisTile({IconData icon,String title})
-  {
-    return Column(
-      children: [
-        Divider(
-          height: 1,
-        ),
-        ListTile(
-          leading: Icon(icon),
-          title: Text(title),
-          trailing: Icon(Icons.arrow_forward),
-        )
-      ],
-    );
+
+  
+  Widget textFromField(String hintText){
+    return Container(
+      height: 50,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.lightBlueAccent[100],
+      ),
+      child: ListTile(
+        leading: Text(hintText),
+      ),
+      );
   }
+   /* return  TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                hintText: hintText,
+                fillColor: Colors.grey[100],
+                border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            );
+  }*/
+
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueAccent,
       appBar: AppBar(
+        leading: IconButton(
+        icon: Icon(Icons.menu, color: Colors.black),
+        onPressed: () => Navigator.of(context).pop(),
+        ), 
         elevation: 0.0,
         title: Text(
           "My Profile",
           style: TextStyle(
             fontSize: 18,
-            color: Colors.deepPurple,
+            color: Colors.black54,
           ),
         ),
-      ),
-      drawer: DashboardPage(),
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 100,
-                color: Colors.blueGrey,
-              ),
-              Container(
-                height: 548,
-                width: double.infinity,
-                padding: EdgeInsets.symmetric(horizontal: 15,vertical: 1),
-                decoration: BoxDecoration(
-                  color: Colors.lightBlueAccent,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  )
-                ),
-                child: ListView(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 250,
-                          height: 80,
-                          padding: EdgeInsets.only(left: 67),
-                          child: Row(
-                            children: [
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "M K Gagan Roshan",
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(height: 10,),
-                                      Text("gagan.cs18@bmsce.ac.in"),
-                                    ],
-                                  ),
-                                  CircleAvatar(
-                                    radius: 15,
-                                    backgroundColor: Colors.yellow,
-                                    child: CircleAvatar(
-                                      radius: 12,
-                                      child: Icon(
-                                        Icons.edit,
-                                        color: Colors.red,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 40,left: 30),
-            child: CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.red,
-            ),
-          )
+        actions: [
+          IconButton(onPressed: (){}, icon: Icon(Icons.edit, color: Colors.black),),
         ],
       ),
+      body: SafeArea(
+        child:  Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                    Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                      backgroundImage: AssetImage("images/non_profile.jpg"),
+                      radius: 50,                    
+                  ),
+                ],
+              ),
+              textFromField("Abhaya"),
+              textFromField("abhayademo@gmail.com"),
+            ],
+          ),
+        ),
+        Expanded(
+            child: Container(),
+            ),
+        ],
+      ),
+    ),
+    )
     );
   }
 }
