708b342526a8ccc2f9309f89965b638e1a823e72
M K GAGAN ROSHAN <53825035+mkgaganr@users.noreply.github.com>
2022-03-18 12:23:48 +0530

    Add files via upload

---
+++ lib/pages/admin/Send_Sms.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sms/flutter_sms.dart';
+
+
+
+List<String> recipents = ["9380417069"];
+
+class SendSms extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Send sms"),),
+      body: Container(
+
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: RaisedButton(
+              color: Theme.of(context).accentColor,
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Text("Send Sms",
+                  style: Theme.of(context).accentTextTheme.button),
+              onPressed: () {
+                _sendSMS("Appointment approved", recipents);
+              },
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+
+
+
+}
+
+void _sendSMS(String message, List<String> recipents) async {
+  String _result = await sendSMS(message: message, recipients: recipents)
+      .catchError((onError) {
+    print(onError);
+  });
+  print(_result);
+}
