25141af1d81f7dc53c7757628ff0a27c48b17ac4
M K GAGAN ROSHAN <53825035+mkgaganr@users.noreply.github.com>
2022-03-22 15:36:37 +0530

    Add files via upload

--- lib/pages/admin/add_appointment.dart
+++ lib/pages/admin/add_appointment.dart
@@ -1,70 +1,146 @@
-import 'package:hmsf_intern/pages/admin/all_data1.dart';
+import 'package:hmsf_intern/pages/Dashboard/dashboard_page.dart';
+import 'package:hmsf_intern/pages/signup/login.dart';
+import 'package:hmsf_intern/pages/welcome/welcome_page.dart';
 import 'package:hmsf_intern/widgets/my_button.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+class AddAppointmentPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
 
-class Addappointment extends StatelessWidget
-{
-  TextEditingController sampledata1 = new TextEditingController();
-  TextEditingController sampledata2 = new TextEditingController();
-  TextEditingController sampledata3 = new TextEditingController();
-  TextEditingController sampledata4 = new TextEditingController();
+class _SignupPageState extends State<AddAppointmentPage> {
+  bool visibility = true;
+  String field1 = "";
+  String field2 = "";
+  String field3 = "";
+  String time="";
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add doctor appointment'
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0.0,
+        title: Text(
+          "Add Appointment",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black54,
+          ),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(40.0),
-        child: Center(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: SingleChildScrollView(
             child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                TextFormField(
-                  controller: sampledata1,
-                  decoration: InputDecoration(
-                      hintText: "doctor name"
-                  ),
+                const SizedBox(
+                  height: 30,
                 ),
+
+
+                const SizedBox(
+                  height: 50,
+                ),
+
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Doctor name",
+                            ),
+                            onChanged: (value) {
+                              field1 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Doctor name required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Hospital description",
+                            ),
+                            onChanged: (value) {
+                              field2 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Hospital description required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "hospital location",
+                            ),
+                            onChanged: (value) {
+                              field3 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Hospital location required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Time",
+                            ),
+                            onChanged: (value) {
+                              time =value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "time required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    )),
+
+                MyButton(
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        Map <String,dynamic> data=
+                        {"doctor name" :field1,
+                          "hospital description":field2,
+                          "hospital location":field3,"time":int.parse(time)};
+                        Firestore.instance.collection("appointment").add(data);}}, text: "add"
+                ),  // MyButton
                 SizedBox(
-                  height: 10.0,
+                  height: 20,
                 ),
-                TextFormField(
-                  controller: sampledata2,
-                  decoration: InputDecoration(
-                      hintText: "hospital location"
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: sampledata3,
-                  decoration: InputDecoration(
-                      hintText: "hospital description"
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: sampledata4,
-                  decoration: InputDecoration(
-                      hintText: "time"
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                MyButton(onPressed: (){Map <String,dynamic> data={"doctor name" :sampledata1.text,"hospital location":sampledata2.text,"hospital description":sampledata3.text,"time":sampledata4.text};
-                Firestore.instance.collection("appointment").add(data);}, text: "add"),
-                SizedBox(
-                  height: 10.0,
-                ),
-                MyButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AppointmentList()));}, text: "update"),
               ],
             ),
           ),
--- lib/pages/admin/add_hospital.dart
+++ lib/pages/admin/add_hospital.dart
@@ -1,115 +1,147 @@
-
+import 'package:hmsf_intern/pages/Dashboard/dashboard_page.dart';
+import 'package:hmsf_intern/pages/signup/login.dart';
+import 'package:hmsf_intern/pages/welcome/welcome_page.dart';
 import 'package:hmsf_intern/widgets/my_button.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
+class AddHospitalsPage extends StatefulWidget {
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
 
-class Addhospital extends StatelessWidget
-{
-  TextEditingController sampledata1 = new TextEditingController();
-  TextEditingController sampledata2 = new TextEditingController();
-  TextEditingController sampledata3 = new TextEditingController();
-  TextEditingController sampledata4 = new TextEditingController();
+class _SignupPageState extends State<AddHospitalsPage> {
+  bool visibility = true;
+  String field1 = "";
+  String field2 = "";
+  String field3 = "";
+  String field4 = "";
+  String noofbeds="";
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add hospitals'
+        leading: IconButton(
+          icon: Icon(Icons.menu, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0.0,
+        title: Text(
+          "Add Hospital",
+          style: TextStyle(
+            fontSize: 18,
+            color: Colors.black54,
+          ),
         ),
       ),
-      body: Container(
-        padding: EdgeInsets.all(40.0),
-        child: Center(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: SingleChildScrollView(
-            key: _formKey,
             child: Column(
-
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                TextFormField(
-                  controller: sampledata1,
-                  decoration: InputDecoration(
-                      hintText: "hospital name"
-                  ),
-                  validator: (value) {
-                    if(value.isEmpty || !RegExp(r'^[a-z A-z]+$').hasMatch(value))
-                      {
-                        return "Data properly";
-                      }
-                    else
-                      {
-                        return null;
-                      }
-                  },
+                const SizedBox(
+                  height: 30,
                 ),
+
+
+                const SizedBox(
+                  height: 50,
+                ),
+
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Hospital name",
+                            ),
+                            onChanged: (value) {
+                              field1 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Hospital name required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "Hospital description",
+                            ),
+                            onChanged: (value) {
+                              field2 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Hospital description required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "hospital location",
+                            ),
+                            onChanged: (value) {
+                              field3 = value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "Hospital location required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: "no of beds",
+                            ),
+                            onChanged: (value) {
+                              noofbeds =value;
+                            },
+                            validator: (value) {
+                              value ??= "";
+                              if (value.trim() == "") {
+                                return "beds required";
+                              }
+                            }),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    )),
+
+                MyButton(
+                        onPressed: (){
+                        if(_formKey.currentState.validate()){
+                       Map <String,dynamic> data=
+                        {"field1" :field1,
+                        "field2":field2,
+                        "field3":field3,"noofbeds":int.parse(noofbeds)};
+                     Firestore.instance.collection("test").add(data);}}, text: "add"
+                ),  // MyButton
                 SizedBox(
-                  height: 10.0,
+                  height: 20,
                 ),
-                TextFormField(
-                  controller: sampledata2,
-                  decoration: InputDecoration(
-                      hintText: "hospital location"
-                  ),
-                  validator: (value) {
-                    if(value.isEmpty || !RegExp(r'^[a-z A-z]+$').hasMatch(value))
-                    {
-                      return "Data properly";
-                    }
-                    else
-                    {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: sampledata3,
-                  decoration: InputDecoration(
-                      hintText: "hospital description"
-                  ),
-                  validator: (value) {
-                    if(value.isEmpty || !RegExp(r'^[a-z A-z]+$').hasMatch(value))
-                    {
-                      return "Data properly";
-                    }
-                    else
-                    {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                TextFormField(
-                  controller: sampledata4,
-                  decoration: InputDecoration(
-                      hintText: "no of beds"
-                  ),
-                  validator: (value) {
-                    if(value.isEmpty || !RegExp(r'^[a-z A-z]+$').hasMatch(value))
-                    {
-                      return "Data properly";
-                    }
-                    else
-                    {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                MyButton(onPressed: () {
-                  if(_formKey.currentState.validate()){
-                  Map <String,dynamic> data={"field1" :sampledata1.text,"field2":sampledata2.text,"field3":sampledata3.text,"noofbeds":sampledata4.text};
-                Firestore.instance.collection("test").add(data);}}, text: "add"),
-                SizedBox(
-                  height: 10.0,
-                ),
-               // MyButton(onPressed: (){Navigator.of(context).push(MaterialPageRoute(builder: (context)=>BookAppointmentPage()));}, text: "update"),
               ],
             ),
           ),
