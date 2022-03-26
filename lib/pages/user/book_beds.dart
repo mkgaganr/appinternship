8c4042f179b9801a6419bafcd140e887add4c75c
M K GAGAN ROSHAN <53825035+mkgaganr@users.noreply.github.com>
2022-03-26 17:22:51 +0530

    Add files via upload

--- lib/pages/user/book_beds.dart
+++ lib/pages/user/book_beds.dart
@@ -13,6 +13,7 @@ class BookBedsPage extends StatefulWidget {
 }
 
 class _BookBedsPageState extends State<BookBedsPage> {
+  TextEditingController _searchcontroller=TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,145 +32,204 @@ class _BookBedsPageState extends State<BookBedsPage> {
           ),
         ),
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('test').snapshots(),
-        builder: (context,AsyncSnapshot snapshot){
-          if (snapshot.hasError) {
-            // handle error
-            return const Text('Error');
-          }
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(
+                          width:300,
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // return progress indicator widget
-            return const Center(child: CircularProgressIndicator());
-          }
-          if(!snapshot.hasData){
-            return CircularProgressIndicator();
-          }
-          return ListView.builder(
-            itemCount: snapshot.data!.docs.length,
-            itemBuilder: (context,index){
-              DocumentSnapshot Hospital = snapshot.data!.docs[index];
-              return Card(
-                color: Colors.lightBlue.shade200,
-                clipBehavior: Clip.hardEdge,
-                child: ListTile(
-                  shape: RoundedRectangleBorder(
-                      side: BorderSide(color: Colors.black, width: 1),
-                      borderRadius: BorderRadius.circular(5),
-                  ),
-                  leading: Icon(Icons.local_hospital,color: Colors.green,),
-                  title: Flexible(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                              child: Row(
-                                children: <Widget>[
-                                  new Text('Hospital name : ',
-                                  style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontSize: 20
-                                  ),),
-                                  new Text(Hospital['field1'], overflow: TextOverflow.clip,
-                                  style: TextStyle(
-                                    color: Colors.blueAccent,
-                                    fontSize: 18
-                                  ),),
-                                  Icon(Icons.local_hospital_outlined,
-                                  color: Colors.blueAccent,)
-                                ],
+                              child: TextFormField(
+                                controller: _searchcontroller,
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(),
+                                    hintText: "Search by Hospital name",
+
+                                ),
                               ),
                             ),
+                        ElevatedButton(onPressed:(){
+                          setState(() {
 
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                new Text('Hospital description : ',
-                                  style: TextStyle(
-                                      color: Colors.blueGrey,
-                                      fontSize: 20
-                                  ),),
-                                new Text(Hospital['field2'], overflow: TextOverflow.clip,
-                                 style: TextStyle(
-                                   color: Colors.blueGrey,
-                                   fontSize: 18
-                                 ),),
-                                Icon(Icons.description_outlined,
-                                  color: Colors.blueGrey,)
-                              ],
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                new Text('Hospital location : ',
-                                  style: TextStyle(
-                                      color: Colors.brown,
-                                      fontSize: 20
-                                  ),),
-                                new Text(Hospital['field3'], overflow: TextOverflow.clip,
-                                  style: TextStyle(
-                                      color: Colors.brown,
-                                      fontSize: 18
-                                  ),),
-                                Icon(Icons.location_on_outlined,
-                                  color: Colors.brown,)
-                              ],
-                            ),
-                          ),
-                          SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Row(
-                              children: [
-                                new Text('No of beds available : ',
-                                  style: TextStyle(
-                                      color: Colors.purple,
-                                      fontSize: 20
-                                  ),),
-                                new Text(Hospital['noofbeds'].toString(), overflow: TextOverflow.clip,
-                                  style: TextStyle(
-                                      color: Colors.purple,
-                                      fontSize: 18
-                                  ),),
-                                Icon(Icons.bed_rounded,
-                                  color: Colors.purple,)
-                              ],
-                            ),
-                          ),
-
-
-
-
-
-
-                          SizedBox(height: 10),
-                          ElevatedButton(onPressed: (){
-                              Navigator.push(context, MaterialPageRoute(builder: (context){
-                              return CommitBookingPage(data: Hospital, id: Hospital.id);
-                        },
+                          });
+                        }, child: Text(
+                          'Search'
+                        )
                         ),
-                        );
-                          }, 
-                          child: new Text("Book",style: TextStyle(
-                            color: Colors.deepPurple,
-                            fontSize: 16
-                          ),),
-                          ),
-                        ],
-
-                      ),
+                      ],
                     ),
-                  ) ,
+                  ],
                 ),
-              );
-            },
-          );
-        },
+            ),
+
+
+            Expanded(
+              child: StreamBuilder(
+                stream: FirebaseFirestore.instance.collection('test').snapshots(),
+                builder: (context,AsyncSnapshot snapshot){
+                  if (snapshot.hasError) {
+                    // handle error
+                    return const Text('Error');
+                  }
+
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // return progress indicator widget
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  if(!snapshot.hasData){
+                    return CircularProgressIndicator();
+                  }
+                  List SearchList=[];
+                  for(var v in snapshot.data.docs){
+                    if(_searchcontroller.text.trim()=="")
+                      {SearchList=snapshot.data.docs;
+                        break;}
+                     else{
+                       if (v.data()[
+                         'field1'
+                       ].contains(_searchcontroller.text.trim())
+
+                       )
+                         {SearchList.add(v);}
+                    }
+                  }
+                  return ListView.builder(
+                    itemCount: SearchList.length,
+                    itemBuilder: (context,index){
+                      DocumentSnapshot Hospital =SearchList[index];
+                      return Card(
+                        color: Colors.lightBlue.shade200,
+                        clipBehavior: Clip.hardEdge,
+                        child: ListTile(
+                          shape: RoundedRectangleBorder(
+                              side: BorderSide(color: Colors.black, width: 1),
+                              borderRadius: BorderRadius.circular(5),
+                          ),
+                          leading: Icon(Icons.local_hospital,color: Colors.green,),
+                          title: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+
+                                children: <Widget>[
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                      child: Row(
+                                        children: <Widget>[
+                                          new Text('Hospital name : ',
+                                          style: TextStyle(
+                                            color: Colors.blueAccent,
+                                            fontSize: 20
+                                          ),),
+                                          new Text(Hospital['field1'], overflow: TextOverflow.clip,
+                                          style: TextStyle(
+                                            color: Colors.blueAccent,
+                                            fontSize: 18
+                                          ),),
+                                          Icon(Icons.local_hospital_outlined,
+                                          color: Colors.blueAccent,)
+                                        ],
+                                      ),
+                                    ),
+
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        new Text('Hospital description : ',
+                                          style: TextStyle(
+                                              color: Colors.blueGrey,
+                                              fontSize: 20
+                                          ),),
+                                        new Text(Hospital['field2'], overflow: TextOverflow.clip,
+                                         style: TextStyle(
+                                           color: Colors.blueGrey,
+                                           fontSize: 18
+                                         ),),
+                                        Icon(Icons.description_outlined,
+                                          color: Colors.blueGrey,)
+                                      ],
+                                    ),
+                                  ),
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        new Text('Hospital location : ',
+                                          style: TextStyle(
+                                              color: Colors.brown,
+                                              fontSize: 20
+                                          ),),
+                                        new Text(Hospital['field3'], overflow: TextOverflow.clip,
+                                          style: TextStyle(
+                                              color: Colors.brown,
+                                              fontSize: 18
+                                          ),),
+                                        Icon(Icons.location_on_outlined,
+                                          color: Colors.brown,)
+                                      ],
+                                    ),
+                                  ),
+                                  SingleChildScrollView(
+                                    scrollDirection: Axis.horizontal,
+                                    child: Row(
+                                      children: [
+                                        new Text('No of beds available : ',
+                                          style: TextStyle(
+                                              color: Colors.purple,
+                                              fontSize: 20
+                                          ),),
+                                        new Text(Hospital['noofbeds'].toString(), overflow: TextOverflow.clip,
+                                          style: TextStyle(
+                                              color: Colors.purple,
+                                              fontSize: 18
+                                          ),),
+                                        Icon(Icons.bed_rounded,
+                                          color: Colors.purple,)
+                                      ],
+                                    ),
+                                  ),
+
+
+
+
+
+
+                                  SizedBox(height: 10),
+                                  ElevatedButton(onPressed: (){
+                                      Navigator.push(context, MaterialPageRoute(builder: (context){
+                                      return CommitBookingPage(data: Hospital, id: Hospital.id);
+                                },
+                                ),
+                                );
+                                  },
+                                  child: new Text("Book",style: TextStyle(
+                                    color: Colors.deepPurple,
+                                    fontSize: 16
+                                  ),),
+                                  ),
+                                ],
+
+                              ),
+                            ),
+                          ) ;
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
